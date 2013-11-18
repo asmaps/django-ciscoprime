c20dd13c955a78ce009a9f44a81c0e2a0e50e7b4
Arne Schauf <github@asmaps.de>
2013-11-18 16:37:01 -0700

    pimping trackedrogue admin

--- ciscoprime/main/admin.py
+++ ciscoprime/main/admin.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 
 from .models import DisabledClient, ClientCount, RogueAP, TrackedRogue
 
+class TrackedRogueAdmin(admin.ModelAdmin):
+    search_fields = ['ap__mac', 'ap__ssid', 'booth_number', 'exhibitor_name']
+    list_display = ('ap', 'booth_number', 'exhibitor_name')
+
 admin.site.register(RogueAP)
-admin.site.register(TrackedRogue)
+admin.site.register(TrackedRogue, TrackedRogueAdmin)
 admin.site.register(ClientCount)
 admin.site.register(DisabledClient)
 
